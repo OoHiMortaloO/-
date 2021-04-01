8e0a0f695cdf3f5d0cfb16dbcb08c33975bed3fc
OoHiMortaloO <arttsai0124@gmail.com>
2021-04-01 14:50:12 +0800

    first push

---
+++ 五子棋.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+
+#define rep(i,n)	for(int i=0; i<n; i++)
+
+void chess_board(){
+		
+return;
+}
+
+bool x_win(){
+	
+}
+
+bool y_win(){
+	
+}
+
+bool r_win(){
+	
+}
+
+bool l_win(){
+	
+}
+
+int main (){
+
+	
+
+return 0;
+}
+
