d545365b8f65d4c8d7aaee983531a417ad872efa
CoKayne <duankayne@gmail.com>
2021-04-29 15:24:12 +0800

    can't change player

--- 五子棋.cpp
+++ 五子棋.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 char board[19][19];
 int backend[19][19];
-int x, y;
+int x = 0, y = 0;
 
 void init_board_backend(){
 	rep(i, 19){
@@ -17,7 +17,7 @@ void init_board_backend(){
 	}
 	for(int i = 0; i < 19; i++){
 		for(int j = 0; j < 19; j++){
-			board[i][j] = '+';
+			board[i][j] = '.';
 		}
 	}
 }
@@ -25,10 +25,18 @@ void init_board_backend(){
 void show_board(){
 	for(int i = 0; i < 19; i++){
 		for(int j = 0; j < 19; j++){
-			cout << board[i][j];
+			if(x == j && y == i) cout << '$' << ' ';
+			else cout << board[i][j] << ' ';
 		}
 		cout << endl;
 	}
+}
+
+void move_back(){
+	if(y > 19) y = 0;
+	if(y < 0) y = 19;
+	if(x > 19) x = 0;
+	if(x < 0) x = 19;
 }
 
 void new_screen(int xpos, int ypos) {
@@ -47,64 +55,64 @@ void new_screen(int xpos, int ypos) {
 bool x_win(char player){
 	int count = 1;
 	for(int i=1;; ++i){
-		if(board[y][x+1] == player){
+		if(board[y][x+i] == player){
 			count++;
 		}else break;
 	}
-	for(int i=1;; ++i){
-		if(board[y][x-1] == player){
+	for(int i=1; x-i >= 0; ++i){
+		if(board[y][x-i] == player){
 			count++;
 		}else break;
 	}
-	if(count >= 5) return true;
+	if(count >= 6) return true;
 	else return false;
 }
 
 bool y_win(char player){
 	int count = 1;
 	for(int i=1;; ++i){
-		if(board[y+1][x] == player){
+		if(board[y+i][x] == player){
 			count++;
 		}else break;
 	}
-	for(int i=1;; ++i){
-		if(board[y-1][x] == player){
+	for(int i=1; y-i >= 0; ++i){
+		if(board[y-i][x] == player){
 			count++;
 		}else break;
 	}
-	if(count >= 5) return true;
+	if(count >= 6) return true;
 	else return false;
 }
 
 bool r_win(char player){
 	int count = 1;
 	for(int i=1;; ++i){
-		if(board[y+1][x+1] == player){
+		if(board[y+i][x+i] == player){
 			count++;
 		}else break;
 	}
-	for(int i=1;; ++i){
-		if(board[y-1][x-1] == player){
+	for(int i=1; (y-i >= 0 && x-i >= 0); ++i){
+		if(board[y-i][x-i] == player){
 			count++;
 		}else break;
 	}
-	if(count >= 5) return true;
+	if(count >= 6) return true;
 	else return false;
 }
 
 bool l_win(char player){
 	int count = 1;
-	for(int i=1;; ++i){
-		if(board[y-1][x+1] == player){
+	for(int i=1; y-i >= 0; ++i){
+		if(board[y-i][x+i] == player){
 			count++;
 		}else break;
 	}
-	for(int i=1;; ++i){
-		if(board[y+1][x-1] == player){
+	for(int i=1; x-i >= 0; ++i){
+		if(board[y+i][x-i] == player){
 			count++;
 		}else break;
 	}
-	if(count >= 5) return true;
+	if(count >= 6) return true;
 	else return false;
 }
 
@@ -150,24 +158,28 @@ cin.tie(0);
 	char player = 'O';
 	// black = O, white = X
 	while(true){
-		int c;
-		c = getch();
-		user_input(c);
-		if(c==114 || c==82){ //R
-			if(check_chess() == false){
-				cout<<"You can't put the chess here!\n";
-			}else{
-				board[y][x] = player; //put chess here
-				backend[y][x] = 1; //marking array
-				player = 'X'; //change player
-			}
-		}
-		if(sb_wins(player) == true || map_full() == true){
-			cout << player << " wins!";
+		if(sb_wins(player) == true){
+			cout << player << " Victory !!!";
 			break;
+		}else{
+			int c;
+			c = getch();
+			user_input(c);
+			move_back();
+			if(c==114 || c==82){ //R
+				if(check_chess() == false){
+					cout<<"You can't put the chess here!\n";
+				}else{
+					board[y][x] = player; //put chess here
+					backend[y][x] = 1; //marking array
+					player = 'X'; //change player
+				}
+			}
+			new_screen(0, 0);
+			show_board();
 		}
-		show_board();
 	}
-
+	new_screen(0, 0);
+	
 return 0;
 }
